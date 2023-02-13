336ffe26b5b8efdb34efbe6e8b05a303671e73e4
Arlen Manasov <arlen.manasov@icloud.com>
2023-02-13 17:41:57 +0600

    fix: removed firebase init. used .env mapbox token

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react"
 
-import districts from './districts.json'
+import districts from "./districts.json"
 
-import { firestore } from './index'
+import { firestore } from "./index"
 
-import Map from './components/Map/Map'
-import { Preloader } from './components/Preloader/index.jsx'
+import Map from "./components/Map/Map"
+import { Preloader } from "./components/Preloader/index.jsx"
 
 const App = () => {
-    const [token, setToken] = useState('')
+  const [token, setToken] = useState("")
 
-    
-    useEffect(() => {
-        const fetchToken = async () => {
-            const response = firestore.collection('firebase-config')
+  useEffect(() => {
+    const fetchToken = async () => {
+      const fetchedToken = process.env.REACT_APP_MAPBOX_TOKEN
 
-            const data = await response.get()
+      setToken(fetchedToken)
+    }
 
-            setToken(data.docs[0].data().token)
-        }
+    fetchToken()
+  }, [])
 
-        fetchToken()
-    }, [])
+  console.log("token:", token)
+  if (!token) return <Preloader />
 
-    console.log('token:', token)
-    if (!token) return <Preloader />
-
-    return (
-        <Map data={districts} token={token} />
-    )
+  return <Map data={districts} token={token} />
 }
 
 export { App }
